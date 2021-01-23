5b7d54d9a67f6d0fb0052c78ab233decfc98b833
Lars Richter <github@n-develop.com>
2021-01-23 21:43:38 +0100

    Added solution with a rough structure

---
+++ src/SimplyDonate.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SimplyDonate.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/SimplyDonate.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SimplyDonate.DataAccess
+{
+    public class Class1
+    {
+    }
+}
