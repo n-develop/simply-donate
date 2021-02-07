1e9d066dbf024dad8bb1cd4fab7b218200ccd44c
Lars Richter <github@n-develop.com>
2021-02-07 14:06:27 +0100

    Adding some sample data to the API. Fake it 'til you make it.

---
+++ src/SimplyDonate.Api/Controllers/DonationsController.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SimplyDonate.Api.Models;
+
+namespace SimplyDonate.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DonationsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Donation> Get()
+        {
+            return Enumerable.Range(1, 22).Select(index => new Donation
+            {
+                Title = $"Gemeinnützige Organisation {index}",
+                Description = "Ich habe einiges abzugeben. Dies und das, sowie dieses und jenes. Erat bibendum etiam, lacinia quisque lacus et. Pulvinar sem fringilla, vulputate non habitasse eu varius. Mauris scelerisque porttitor, fusce torquent non congue erat. A suscipit metus, torquent quam, aenean eget libero sapien himenaeos lorem mauris. Ullamcorper donec netus, vitae per venenatis augue aenean. Fermentum risus libero, nec nisl lobortis metus urna. Mi rhoncus arcu, felis ipsum nibh tristique.",
+            });
+        }
+    }
+}
---
+++ src/SimplyDonate.Api/Models/NonProfit.cs
@@ -0,0 +1,13 @@
+namespace SimplyDonate.Api.Models
+{
+    public class NonProfit
+    {
+        public string Name { get; set; }
+        
+        public string Email { get; set; }
+        
+        public string Description { get; set; }
+        
+        public string Address { get; set; }
+    }
+}
