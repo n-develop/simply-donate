e9ddf4e7d0a975ec2ea6bb7243ca8670130a32bf
Lars Richter <github@n-develop.com>
2021-02-21 20:14:19 +0100

    Ensure existence of NonProfitController

--- src/SimplyDonate.Api.Tests/Controllers/NonProfitControllerTests/GetTests.cs
+++ src/SimplyDonate.Api.Tests/Controllers/NonProfitControllerTests/GetTests.cs
@@ -1,7 +1,29 @@
+using SimplyDonate.Api.Controllers;
+using Xunit;
+
 namespace SimplyDonate.Api.Tests.Controllers.NonProfitControllerTests
 {
     public class GetTests
     {
-        
+        [Fact]
+        public void ItExists()
+        {
+            // Arrange
+            // Act
+            var controller = new NonProfitController();
+            // Assert
+        }
+
+        [Fact]
+        public void ItHasGet()
+        {
+            // Arrange
+            var controller = new NonProfitController();
+
+            // Act
+            controller.Get();
+
+            // Assert
+        }
     }
 }
