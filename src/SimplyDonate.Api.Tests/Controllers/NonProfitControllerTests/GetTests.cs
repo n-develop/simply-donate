a1a0d489e6dd6a1220b80c9cee1fc88ee1b87eb6
Lars Richter <github@n-develop.com>
2021-02-21 20:20:24 +0100

    Ensure correct type of returned value

--- src/SimplyDonate.Api.Tests/Controllers/NonProfitControllerTests/GetTests.cs
+++ src/SimplyDonate.Api.Tests/Controllers/NonProfitControllerTests/GetTests.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using SimplyDonate.Api.Controllers;
+using SimplyDonate.Api.Models;
 using Xunit;
 
 namespace SimplyDonate.Api.Tests.Controllers.NonProfitControllerTests
@@ -18,6 +20,20 @@ namespace SimplyDonate.Api.Tests.Controllers.NonProfitControllerTests
 
             // Assert
             result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public void ItReturnsCollectionOfNonProfits()
+        {
+            // Arrange
+            var controller = new NonProfitController();
+
+            // Act
+            var result = controller.Get() as OkObjectResult;
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Value.Should().BeAssignableTo<IEnumerable<NonProfit>>();
         }
     }
 }
--- src/SimplyDonate.Api/Controllers/NonProfitController.cs
+++ src/SimplyDonate.Api/Controllers/NonProfitController.cs
@@ -17,7 +17,7 @@ namespace SimplyDonate.Api.Controllers
                 Description = "Hier wird viel Gutes getan. Dies und das, sowie dieses und jenes. Erat bibendum etiam, lacinia quisque lacus et. Pulvinar sem fringilla, vulputate non habitasse eu varius. Mauris scelerisque porttitor, fusce torquent non congue erat. A suscipit metus, torquent quam, aenean eget libero sapien himenaeos lorem mauris. Ullamcorper donec netus, vitae per venenatis augue aenean. Fermentum risus libero, nec nisl lobortis metus urna. Mi rhoncus arcu, felis ipsum nibh tristique.",
                 Address = $"Hamburg Str. {index}\n3628{index} Mannheim",
                 Email = $"number{index}@awesome.nonprofit"
-            });
+            }).ToList();
             return Ok(nonProfits);
         }
     }
