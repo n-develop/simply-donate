40acd82ac23539b90e9481ff77642e4fca596dbd
Lars Richter <github@n-develop.com>
2021-02-21 20:05:58 +0100

    Remove old tests that do not add value anymore

--- src/SimplyDonate.Api.Tests/Controllers/DonationsControllerTests/GetTests.cs
+++ src/SimplyDonate.Api.Tests/Controllers/DonationsControllerTests/GetTests.cs
@@ -8,22 +8,6 @@ namespace SimplyDonate.Api.Tests.Controllers.DonationsControllerTests
     public class GetTests
     {
         [Fact]
-        public void ItExists()
-        {
-            var controller = new DonationsController();
-        }
-
-        [Fact]
-        public void ItHasGet()
-        {
-            // Arrange
-            var controller = new DonationsController();
-
-            // Act
-            controller.Get();
-        }
-
-        [Fact]
         public void ItReturnsOkObjectResult()
         {
             // Arrange
