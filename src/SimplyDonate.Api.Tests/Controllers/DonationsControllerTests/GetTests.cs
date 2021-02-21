66f8f2b7297499e2b1900648229f5f78ff37636e
Lars Richter <github@n-develop.com>
2021-02-21 20:12:40 +0100

    Ensure correct type of returned value

--- src/SimplyDonate.Api.Tests/Controllers/DonationsControllerTests/GetTests.cs
+++ src/SimplyDonate.Api.Tests/Controllers/DonationsControllerTests/GetTests.cs
@@ -1,23 +1,42 @@
+using System.Collections.Generic;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using SimplyDonate.Api.Controllers;
+using SimplyDonate.Api.Models;
 using Xunit;
 
 namespace SimplyDonate.Api.Tests.Controllers.DonationsControllerTests
 {
     public class GetTests
     {
+        private readonly DonationsController _controller;
+        
+        public GetTests()
+        {
+            _controller = new DonationsController();
+        }
+        
         [Fact]
         public void ItReturnsOkObjectResult()
         {
             // Arrange
-            var controller = new DonationsController();
-
             // Act
-            var result = controller.Get();
+            var result = _controller.Get();
             
             // Assert
             result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public void ItReturnsCollectionOfDonations()
+        {
+            // Arrange
+            // Act
+            var result = _controller.Get() as OkObjectResult;
+            
+            // Assert
+            result.Should().NotBeNull();
+            result.Value.Should().BeAssignableTo<IEnumerable<Donation>>();
         }
     }
 }
--- src/SimplyDonate.Api/Controllers/DonationsController.cs
+++ src/SimplyDonate.Api/Controllers/DonationsController.cs
@@ -16,7 +16,7 @@ namespace SimplyDonate.Api.Controllers
             {
                 Title = $"Coole Sachspende Nr. {index}",
                 Description = "Ich habe einiges abzugeben. Dies und das, sowie dieses und jenes. Erat bibendum etiam, lacinia quisque lacus et. Pulvinar sem fringilla, vulputate non habitasse eu varius. Mauris scelerisque porttitor, fusce torquent non congue erat. A suscipit metus, torquent quam, aenean eget libero sapien himenaeos lorem mauris. Ullamcorper donec netus, vitae per venenatis augue aenean. Fermentum risus libero, nec nisl lobortis metus urna. Mi rhoncus arcu, felis ipsum nibh tristique.",
-            });
+            }).ToList();
             
             return Ok(donations);
         }
